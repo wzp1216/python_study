2ba781e8e5e2580236dec20cfb691743fec141ca
wzp1216 <wzp1216@163.com>
2019-02-11 02:38:33 +0800

    show pic is ok

--- pythonstudy/pyqt/maintest.py
+++ pythonstudy/pyqt/maintest.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtWidgets import QFileDialog,QGraphicsScene
-from PyQt5.QtGui import QImage;
+from PyQt5.QtGui import QImage,QPixmap,QBrush;
+from PyQt5.QtWidgets import QMessageBox
 from main import Ui_MainWindow
 
 class mywin(QMainWindow,Ui_MainWindow):
@@ -19,14 +20,17 @@ class mywin(QMainWindow,Ui_MainWindow):
         self.lcdNumber.show()
         filedlg=QFileDialog()
         filename=filedlg.getOpenFileName(self)
-        print(filename)
-        filename=self.debuglabel.setText(str(filename[0]))
-        myimg=QImage()
-
-        myimg.load(filename)
-        sence=QGraphicsScene();
-        sence.addPixmap()
-        self.graphicsView.show();
+        filename=str(filename[0])
+        self.debuglabel.setText(filename)
+        image=QImage();
+        if(not (image.load(filename))):
+            QMessageBox.information(self,"error","open file is erroe");
+            return;
+        pixmap=QPixmap()
+        pixmap.convertFromImage(image)
+        self.labelpic.setPixmap(pixmap);
+        self.labelpic.resize(800,600);
+        self.labelpic.show()
 
     def tfpic(self):
         self.lcdNumber.setDigitCount(3)
